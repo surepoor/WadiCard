555bc04ced0574af9fe80aa0d4ce607bded6ebab
kha09 <dev.khalid.0@gmail.com>
2022-01-31 17:56:21 +0300

    added new models need to fetch it next

--- newapp/admin.py
+++ newapp/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Hero)
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(Ad)
--- newapp/models.py
+++ newapp/models.py
@@ -1,7 +1,58 @@
 from django.db import models
+from datetime import datetime
+from multiselectfield import MultiSelectField
+
+
+city_choices = (
+	('مكة','مكة'),
+	('الرياض','الرياض'),
+	('جدة','جدة'),
+	('المدينة المنورة','المدينة المنورة'),
+	('الدمام','الدمام'),
+	('الطائف','الطائف'),
+	('حائل','حائل'),
+	('ينبع','ينبع'),
+
+)
 
 # Create your models here.
 class Hero(models.Model):
 	image_hero = models.ImageField(upload_to='photos/%y/%m/%d')
 	logo_header = models.ImageField(upload_to='photos/%y/%m/%d')
 	join_us = models.URLField(max_length=150)
+
+
+
+class Category(models.Model):
+	logo_cat = models.ImageField(upload_to='photos/category')
+	name_cat = models.CharField(max_length=50)
+	desc_cat = models.CharField(max_length=150)
+	date_created = models.DateTimeField(default=datetime.now(), blank=True)
+
+
+class Company(models.Model):
+	logo_com = models.ImageField(upload_to='photos/company')
+	name_com = models.CharField(max_length=50)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	is_featured = models.BooleanField(default=False)
+	desc_com = models.CharField(max_length=150)
+	location = models.CharField(max_length=70)
+	city = MultiSelectField(choices= city_choices)
+	phone = models.IntegerField(max_length=15)
+	website = models.URLField(max_length=150)
+	facebook_link = models.URLField(max_length=100)
+	twitter_link = models.URLField(max_length=100)
+	instgram_link = models.URLField(max_length=100)
+	linkedin_link = models.URLField(max_length=100)
+	whatsapp_link = models.URLField(max_length=100)
+	email = models.EmailField(max_length=255)
+	date_created = models.DateTimeField(default=datetime.now(), blank=True)
+	end_date = models.DateTimeField()
+
+class Ad(models.Model):
+	image_ad = models.ImageField(upload_to='photos/ad')
+	link = models.URLField(max_length=150)
+	date_created = models.DateTimeField(default=datetime.now(), blank=True)
+	end_date = models.DateTimeField()
+
+
