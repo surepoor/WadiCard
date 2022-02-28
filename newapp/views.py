a40c3ee8622b423018d7b43cc75cb27e6910ab52
kha09 <dev.khalid.0@gmail.com>
2022-02-28 15:23:25 +0300

    added cloudinary working locally, debug=False

--- newapp/views.py
+++ newapp/views.py
@@ -85,3 +85,9 @@ def register(request):
 
 	else:
 		return render(request, 'register.html')
+
+
+def view_404(request, exception=None):
+	# make a redirect to homepage
+    # you can use the name of url or just the plain link
+    return redirect('home') # or redirect('name-of-index-url')
